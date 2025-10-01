f1986c9290e135d1a22addf79bd1d4828b021688
Farooqui007 <farooquianas007@gmail.com>
2025-10-01 16:31:05 +0530

    commit

--- DSA1.java
+++ DSA1.java
@@ -1,13 +1,14 @@
 /*
-Given an integer array nums, find the subarray with the largest sum and return the sum of the elements present in that subarray.
-A subarray is a contiguous non-empty sequence of elements within an array.
-Examples:
-Input: nums = [2, 3, 5, -2, 7, -4]
-Output: 15
-Explanation: The subarray from index 0 to index 4 has the largest sum = 15
-Input: nums = [-2, -3, -7, -2, -10, -4]
-Output: -2
-Explanation: The element on index 0 or index 3 make up the largest sum when taken as a subarray
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+The test cases are generated so that the answer will fit in a 32-bit integer.
+Example 1:
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 */
 class DSA1 {
     public int maxSubArray(int[] nums) {
@@ -15,9 +16,9 @@ class DSA1 {
         int max = Integer.MIN_VALUE;
 
         for(int i=0;i<n;i++){
-            int sum = 0;
+            int sum = 1;
             for(int j=i;j<n;j++){
-                sum = sum + nums[j];
+                sum = sum * nums[j];
                 max = Math.max(max,sum);
             }
 
@@ -25,7 +26,7 @@ class DSA1 {
         return max;
     }
     public static void main (String args[]){
-        int [] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int [] nums = { -2, 1, -3, 4, -1, 2, 1, -5, -4};
         DSA1 obj = new DSA1();
        int maximum = obj.maxSubArray(nums);
        System.out.println("maximum is " + maximum );
